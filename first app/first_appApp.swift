0005b2e695a5355f13bd8cd632a9095226b7ff57
ifra <ifraeishaq@gmail.com>
2023-06-26 19:59:10 +0530

    Initial Commit

---
+++ first app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first app
+//
+//  Created by Ifra Ishaq on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
